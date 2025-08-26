2d2d5e5791e91a686336cd6649f394be8e8e0204
Emdad94 <emdadul.cse94@gmail.com>
2025-08-26 11:16:53 +0600

    making responsive

--- attendence.php
+++ attendence.php
@@ -15,11 +15,32 @@ else{
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/attendence.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello</h1>
-    <button id="btnLogout">LOGOUT</button>
+   <!-- <h1>Hello</h1>
+    <button id="btnLogout">LOGOUT</button>-->
+
+    <div class="page">
+        <div class="header-area">
+            <div class="logo-area"><h1 class="logo">ATTENDANCE APP</h1></div>
+            <div class="logout-area"><button class="btnlogout">LOGOUT</button></div>
+        </div>
+        <div class="session-area">
+            <div class="label-area"><label>SESSION</label></div>
+            <div class="dropdown-area">
+                <select class="ddlclass">
+                    <option>SELECT ONE</option>
+                    <option>2025 AUTUMN</option>
+                    <option>2025 SPRING</option>
+                </select>
+            </div>
+        </div>
+        <div class="classlist-area">3</div>
+        <div class="classdetails-area">4</div>
+        <div class="studentlist-area">5</div>
+    </div>
     <script src="js/jquery.js"></script>
     <script src="js/logout.js"></script>
 </body>
